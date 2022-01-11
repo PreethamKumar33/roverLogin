92b4f6378fa174706d319ebb3f0231ef34a8a220
Rishabh Gupta <rishabhgupta@Rishabhs-MacBook-Pro.local>
2022-01-11 09:27:12 +0530

    modified loader

--- core/nlp/plugins/loader.py
+++ core/nlp/plugins/loader.py
@@ -7,11 +7,12 @@ plugin_dict = {}
 
 
 def import_module(plugin_name):
-    if plugin_dict.get(plugin_name) is not None and plugin_dict.get(plugin_name) > os.path.getmtime(
+    if plugin_dict.get(plugin_name) is not None and plugin_dict.get(plugin_name)["timestamp"] > os.path.getmtime(
             "plugin_config.py"):
-        return plugin_dict[plugin_name]
-    plugin_dict[plugin_name] = time.time()
-    return importlib.import_module("plugins." + plugin_config.config.get(plugin_name))
+        return plugin_dict[plugin_name]["module"]
+    plugin_dict[plugin_name] = {"module" : importlib.import_module("plugins." + plugin_config.config.get(plugin_name)),
+                                "timestamp" : time.time()}
+    return plugin_dict[plugin_name]["module"]
 
 
 def load_modules():
